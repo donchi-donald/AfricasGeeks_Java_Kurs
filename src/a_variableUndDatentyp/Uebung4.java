72706eef1ae800fc0fd5186cafb7caf1c3308907
donchi-fofack <donchidonald2000@gmail.com>
2021-08-23 09:32:34 +0200

    klein update

--- src/a_variableUndDatentyp/Uebung4.java
+++ src/a_variableUndDatentyp/Uebung4.java
@@ -1,5 +1,7 @@
 package a_variableUndDatentyp;
 
+import java.util.Scanner;
+
 /*
  Schreiben Sie ein Java-Programm, das die x- und y-Koordinaten zweier Punkte einliest und den Abstand zwischen ihnen berechnet und ausgibt.
 
@@ -8,7 +10,18 @@ Tipp: Math.sqrt() berechnet eine Wurzel
 public class Uebung4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Scanner clavier =new Scanner (System.in);
+         System.out.println("geben sie xa");
+         int xa= clavier.nextInt();
+         System.out.println("geben sie ya");
+         int ya= clavier.nextInt();
+         System.out.println("geben sie xb");
+         int xb= clavier.nextInt();
+         System.out.println("geben sie yb");
+         int yb= clavier.nextInt();
+         int xbMinusXaQuadrat=(xb-xa)*(xb-xa);
+         int ybMinusYaQuadrat=(yb-ya)*(yb-ya);
+         System.out.println("Abstand=wuerzel(xbMinusXaQuadrat + ybMinusYaQuadrat): "+Math.sqrt(xbMinusXaQuadrat+ybMinusYaQuadrat));
 
 	}
 
